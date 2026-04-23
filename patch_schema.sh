a632f4b0204afacd400881688f90079129f5cbc6
MohammadAli1010 <mohammadali991001@gmail.com>
2026-04-23 17:52:44 +0530

    Remove obsolete patch scripts for authentication and schema updates

--- patch_schema.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Insert enum Role
-sed -i '/enum JobStatus/a \
-\
-enum Role {\n  USER\n  MODERATOR\n  ADMIN\n}' prisma/schema.prisma
-
-# Insert role field to User model
-sed -i '/reputationScore Int/a \  role            Role            @default(USER)' prisma/schema.prisma
--- patch_schema_guides.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i '/coverImageUrl String?/a \  isPublished   Boolean  @default(false)' prisma/schema.prisma
-npx prisma format
-npx prisma generate
